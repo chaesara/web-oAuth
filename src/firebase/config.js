f72408c0b9f8e285668499e7fdd4f9eb2d77ae73
chaesara <chaesaraditya0979@gmail.com>
2026-06-04 13:11:01 +0700

    tambah env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmIYCRpf6aTY26JkSFxwlWRtlvn4RVT10",
-  authDomain: "oauthpro-1d4ff.firebaseapp.com",
-  projectId: "oauthpro-1d4ff",
-  storageBucket: "oauthpro-1d4ff.firebasestorage.app",
-  messagingSenderId: "827340053829",
-  appId: "1:827340053829:web:503a41232d2c30f1a6322c",
-  measurementId: "G-26VMRK7TY8",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
